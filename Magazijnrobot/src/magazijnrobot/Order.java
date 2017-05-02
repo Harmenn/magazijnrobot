1c765b976bc5fc1d162507c3cde7855da7424dcb
harmenn <harmen_meijden@hotmail.com>
2017-05-02 11:42:06 +0200

    Clean up

--- Magazijnrobot/src/magazijnrobot/Order.java
+++ Magazijnrobot/src/magazijnrobot/Order.java
@@ -19,26 +19,6 @@ public class Order {
 	}
 	
 	public ArrayList<Product> getProducten() {
-		/*
-		//Object[][] data = {{},{},{}};
-		int c = 0;
-		
-		Object[][] data = {
-		    {"1", "Lego doos",
-		     "€5,00", new Integer(5), "Opgehaalt"},
-		    {"2", "Lego tas",
-		     "€3,00", new Integer(3), "Kwijt geraakt"}
-		};
-		
-		for(Product i : producten) {
-			data[c][0] = i.getId();
-			data[c][1] = i.getOmschrijving();
-			data[c][2] = i.getPrijs();
-			data[c][3] = i.getGrootte();
-			data[c][4] = i.getStatus();
-			c++;
-		}
-		return data;*/
 		return producten;
 	}
 }
