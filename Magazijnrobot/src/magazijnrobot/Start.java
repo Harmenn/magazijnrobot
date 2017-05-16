42cc293b07c9b726970010f0e68ba532085763a3
leroyvandijk <s1104794@windesheim.nl>
2017-05-16 15:04:12 +0200

    Terug naar origineel

--- Magazijnrobot/src/magazijnrobot/Start.java
+++ Magazijnrobot/src/magazijnrobot/Start.java
@@ -7,8 +7,7 @@ public class Start {
 	public static void main(String[] args) throws IOException {
 		SerialEvent event = new SerialEvent();
 
-		event.sendMessage("commando left");
+		event.sendMessage("test");
                 
-                event.disconnect();
 	}
 }
