a95da7ddedc7b862a97923c9ef4eb923df9def0c
leroyvandijk <s1104794@windesheim.nl>
2017-05-18 15:10:10 +0200

    BPP DrawPanel fixed

--- Magazijnrobot/src/magazijnrobot/BPP_DrawPanel.java
+++ Magazijnrobot/src/magazijnrobot/BPP_DrawPanel.java
@@ -6,33 +6,30 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-import magazijnrobot.*;
-
 public class BPP_DrawPanel extends JPanel {
     
     LiveView result;
-    private int x = 50, y = 90, yProduct = 90, volume = 0, volumeDozen = 0, BinSize = 0;
+    private int x, y, yProduct, volumeDozen, BinSize;
     ArrayList<Bin> bins = new ArrayList<>();
 
-    public BPP_DrawPanel(LiveView result, ArrayList<Bin> Bins, int Volume, int BinSize) {
+    public BPP_DrawPanel(LiveView result, ArrayList<Bin> Bins, int BinSize) {
         this.result = result;
         this.bins = Bins;
-        this.volume = Volume;
         this.volumeDozen = BinSize * Bins.size();
         this.BinSize = BinSize;
-        int prefsize = (Bins.size() / 10) * (BinSize * 10) + (Bins.size() / 10 * 50);
+        int prefsize = (Bins.size() / 3) * (BinSize * 10) + (Bins.size() / 3 * 100);
         this.setIgnoreRepaint(true);
-        this.setPreferredSize(new java.awt.Dimension(1200, prefsize + 100));
+        this.setPreferredSize(new java.awt.Dimension(350, prefsize + 100));
     }
     
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        x = 50;
+        x = 20;
         y = 90;
         yProduct = 90;
         g.drawString("Aantal dozen: " + bins.size(), x, 40);
-        g.drawString("Totaal volume producten: " + volume + "     Totaal volume dozen: " + volumeDozen, x, 60);
+        g.drawString("Totaal volume dozen: " + volumeDozen, x, 60);
         
         for (Bin bin : bins) {
             g.setColor(Color.BLACK);
@@ -51,9 +48,9 @@ public class BPP_DrawPanel extends JPanel {
                 g.drawString(Integer.toString(product.getVolume()), x + 55, yProduct);
             }
             x += 100;
-            if (x > 1000) {
+            if (x > 250) {
                 y += (BinSize * 10) + 50;
-                x = 50;
+                x = 20;
             }
         }
         
