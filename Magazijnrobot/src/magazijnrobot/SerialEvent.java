108bded48dff7dd5cafe69b68fdd96109ba2768f
leroyvandijk <s1104794@windesheim.nl>
2017-05-22 15:48:37 +0200

    Might work

--- Magazijnrobot/src/magazijnrobot/SerialEvent.java
+++ Magazijnrobot/src/magazijnrobot/SerialEvent.java
@@ -11,6 +11,7 @@ import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
+import java.util.ArrayList;
 import tsp_simulator.Coordinate;
 
 public class SerialEvent implements SerialPortEventListener {
@@ -55,6 +56,7 @@ public class SerialEvent implements SerialPortEventListener {
 		try {
 
 			BufferedReader input = null;
+                        ArrayList<Product> pickedProducts = new ArrayList<>();
 			if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
 				if (input == null) {
 					input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
@@ -80,6 +82,19 @@ public class SerialEvent implements SerialPortEventListener {
 						} else if (splitted[2].equals("at_y_2")) {
 							StartScherm.bpp_connectie.sendMessage("command-arm_all_in");
 						}
+                                                for (Product product : pickedProducts) {
+                                                    for (Bin bin : StartScherm.binlist) {
+                                                        if(bin.getProducts().contains(product)){
+                                                            if(bin.rechts){
+                                                                StartScherm.bpp_connectie.sendMessage("commando-rotate_right");
+                                                                System.out.println("RECHTS");
+                                                            } else{
+                                                                 StartScherm.bpp_connectie.sendMessage("commando-rotate_left");
+                                                                 System.out.println("LINKS");
+                                                            }
+                                                        }
+                                                    }                                               
+                                                }
 					}
 				} else if (splitted[0].equals("bpp")) {
 					//System.out.println("DEBUG: REACHED BPP");
@@ -100,6 +115,7 @@ public class SerialEvent implements SerialPortEventListener {
 								StartScherm.tsp_connectie.sendMessage("getproduct-"
 										+ StartScherm.producten.get(lastP).getX() + "-"
 										+ StartScherm.producten.get(lastP).getY());
+                                                                pickedProducts.add(StartScherm.producten.get(lastP));
 							}
 						}
 
